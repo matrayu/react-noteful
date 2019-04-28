8368bbb7d0a01552b7e7bc7a9e46124a9e55fc5f
matrayu <northridge614@gmail.com>
2019-04-28 13:12:35 -0700

    Update NavNote.test.js
    Added BrowserRouter to test

--- src/components/NavNote/NavNote.test.js
+++ src/components/NavNote/NavNote.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import NavNote from './NavNote';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NavNote />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <NavNote />
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
