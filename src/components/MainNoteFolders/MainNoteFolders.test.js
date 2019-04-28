246114dc3ad84eaafa7f20072ac4c4c251cb9880
matrayu <northridge614@gmail.com>
2019-04-28 13:12:45 -0700

    Update MainNoteFolders.test.js
    Added BrowserRouter to test

--- src/components/MainNoteFolders/MainNoteFolders.test.js
+++ src/components/MainNoteFolders/MainNoteFolders.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainNoteFolders from './Note';
+import { BrowserRouter } from 'react-router-dom'
+import MainNoteFolders from './MainNoteFolders';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MainNoteFolders />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <MainNoteFolders />
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
