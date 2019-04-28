fb9114828b0532b329e6a570493d0a0befa480f3
matrayu <northridge614@gmail.com>
2019-04-28 13:12:25 -0700

    Update MainNote.test.js
    Added BrowserRouter to test

--- src/components/MainNote/MainNote.test.js
+++ src/components/MainNote/MainNote.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainNote from './MainNote';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MainNote />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <MainNote />
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
