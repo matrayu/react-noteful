ae738ad56086001ce52b8117da4bf0b615fa26de
matrayu <northridge614@gmail.com>
2019-04-28 13:12:39 -0700

    Update NavFolders.test.js
    Added BrowserRouter to test

--- src/components/NavFolders/NavFolders.test.js
+++ src/components/NavFolders/NavFolders.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import NavFolders from './NavFolders';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NavFolders />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <NavFolders />
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
