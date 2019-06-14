af7079a7dfee91988a32cf66ed13f81686c84e19
matrayu <northridge614@gmail.com>
2019-06-14 12:22:52 -0700

    Update MainNoteFolders.test.js

--- src/components/MainNoteFolders/MainNoteFolders.test.js
+++ src/components/MainNoteFolders/MainNoteFolders.test.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import MainNoteFolders from './MainNoteFolders';
 
+const defaultProps = {
+  location: '/'
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <MainNoteFolders />
+      <MainNoteFolders {...defaultProps}/>
     </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
