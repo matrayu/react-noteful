9ea8486a2f477eb07dd56e6c457adb751f316061
matrayu <northridge614@gmail.com>
2019-06-14 06:43:01 -0700

    Update MainNoteFolders.js
    Added ability to delete folders and notes within the folder being deleted

--- src/components/MainNoteFolders/MainNoteFolders.js
+++ src/components/MainNoteFolders/MainNoteFolders.js
@@ -35,7 +35,7 @@ export default class MainNoteFolders extends React.Component {
                     throw error
                 })
             }
-            return res.json()
+            return res
         })
         .then(data => {
             this.context.deleteNote(noteId)
@@ -45,31 +45,56 @@ export default class MainNoteFolders extends React.Component {
         })
     }
 
+    handleFolderDeleteRequest = (folderId, folderNotes, e) => {
+        e.preventDefault();
+        fetch(`${config.API_ENDPOINT}/folders/${folderId}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => {
+            if (!res.ok) {
+                return res.json().then(error => {
+                    throw error
+                })
+            }
+            return res
+        })
+        .then(data => {
+            this.context.deleteFolder(folderId, folderNotes, () => {
+                this.props.history.push('/')
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
     render() {
         const { notes } = this.context;
-
+        const { folderId } = this.props.match.params
+        
         const getFolderNotes = (notes=[], folderId) => (
             (!folderId) 
               ? notes 
-              : notes.filter(note => note.folderId === folderId)
+              : notes.filter(note => note.folder_id == folderId)
         );
-        
-        const { folderId } = this.props.match.params
         const folderNotes = getFolderNotes(notes, folderId);
 
         return (
             <div className='MainNoteFolders'>
                 <ul className='MainNoteFolders__list'>
-                    {folderNotes.map(note => 
+                    {folderNotes.map(note =>
                         <li className='MainNoteFolders' id={note.id} key={note.id}>
                             <div className='MainNoteFolders__row'>
                                 <div className="MainNoteFolders__title">
-                                    <Link to={`/note/${note.id}`}>
-                                        <h2>{note.name}</h2>
+                                    <Link to={`/notes/${note.id}`}>
+                                        <h2>{note.title}</h2>
                                     </Link>
                                 </div>
                                 <div className='MainNoteFolders__modified'>
-                                    Created: {format(note.modified, 'Do MMM YYYY')}
+                                    Created: {format(note.date_modified, 'Do MMM YYYY')}
                                 </div>
                             </div>
                             <div className='MainNoteFolders__button'>
@@ -87,6 +112,14 @@ export default class MainNoteFolders extends React.Component {
                     <Link to={'/add-note'}>
                         <button className="MainNoteFolders__addNote_button">Add Note</button>
                     </Link>
+                    <div className='MainNoteFolders__deleteFolder'>
+                        <button 
+                            className='MainNoteFolders__deleteFolder_button'
+                            onClick={this.handleFolderDeleteRequest.bind(this, folderId, folderNotes)}
+                        >
+                            Delete Folder
+                        </button>
+                    </div>
                 </div>
             </div>
         )
